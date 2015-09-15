683e29425cf9d1805a94903d4a0c61bb366d5898
Neil Scudder <neil@neilscudder.com>
2015-09-15 12:23:50 -0700

    Cleanup

--- control.php
+++ control.php
@@ -113,24 +113,24 @@ if (isset($GETA)) {
       foreach ($results as &$result) {
         $resultEncoded=rawurlencode($result);
         echo "
-          <li class=\"full\">
+          <li class='full'>
             $result
-            <div class=\"playbtn\">
-              <svg version=\"1.1\" 
-                class=\"animated play\" 
-                id=\"$resultEncoded\" 
-                xmlns=\"http://www.w3.org/2000/svg\" 
-                xmlns:xlink=\"http://www.w3.org/1999/xlink\" 
-                x=\"0px\" 
-                y=\"0px\"
-                viewBox=\"0 0 512 512\" 
-                enable-background=\"new 0 0 512 512\" 
-                xml:space=\"preserve\">
+            <div class='playbtn'>
+              <svg version='1.1' 
+                class='animated play' 
+                id='$resultEncoded' 
+                xmlns='http://www.w3.org/2000/svg' 
+                xmlns:xlink='http://www.w3.org/1999/xlink' 
+                x='0px' 
+                y='0px'
+                viewBox='0 0 512 512' 
+                enable-background='new 0 0 512 512' 
+                xml:space='preserve'>
                <g>
-                <path class=\"playPath\" d=\"M508.6,148.8c0-45-33.1-81.2-74-81.2C379.2,65,322.7,64,265,64c-3,0-6,0-9,0s-6,0-9,0
+                <path class='playPath' d='M508.6,148.8c0-45-33.1-81.2-74-81.2C379.2,65,322.7,64,265,64c-3,0-6,0-9,0s-6,0-9,0
                   c-57.6,0-114.2,1-169.6,3.6C36.6,67.6,3.5,104,3.5,149C1,184.6-0.1,220.2,0,255.8C-0.1,291.4,1,327,3.4,362.7
                   c0,45,33.1,81.5,73.9,81.5c58.2,2.7,117.9,3.898,178.6,3.8c60.8,0.2,120.3-1,178.6-3.8c40.9,0,74-36.5,74-81.5
-                  c2.4-35.7,3.5-71.3,3.4-107C512.1,220.1,511,184.5,508.6,148.8z M207,353.9V157.4l145,98.2L207,353.9z\"/>
+                  c2.4-35.7,3.5-71.3,3.4-107C512.1,220.1,511,184.5,508.6,148.8z M207,353.9V157.4l145,98.2L207,353.9z'/>
                </g>
               </svg>
             </div>
