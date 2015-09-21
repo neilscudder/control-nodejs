1be3b44995013d156402ab3478b57301ace61675
Neil Scudder <neil@neilscudder.com>
2015-09-21 18:21:26 -0400

    fuckin git

--- index.php
+++ index.php
@@ -44,10 +44,7 @@ if (isset($_GET["l"])) {
         <svg 
           class="toolbar" 
           id="dn" 
-          version="1.1" 
           id="Layer_1" 
-          xmlns="http://www.w3.org/2000/svg" 
-          xmlns:xlink="http://www.w3.org/1999/xlink" 
           x="0px" 
           y="0px"
           viewBox="0 0 24 24" 
