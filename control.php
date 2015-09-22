633bd41f32c051090b72bcef3acc662149ef0fbf
Neil Scudder <neil@neilscudder.com>
2015-09-22 10:27:24 -0700

    Moving code to functions

--- control.php
+++ control.php
@@ -8,6 +8,8 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
+$cacheDir="cache/";
+
 function authenticate() {
   $m = new MongoClient("mongodb://webserver:webmunster@localhost/authority");
   $db = $m->authority;
@@ -21,7 +23,64 @@ function authenticate() {
   }
 }
 
-$cacheDir="cache/";
+function showInfo(){
+  $cacheFile = "$cacheDir/$MPDPORT.cache";
+  $comparisonFile = "$cacheDir/$MPDPORT.comparison";
+
+  if (file_exists($comparisonFile)) {
+    $previousFname = shell_exec("cat $comparisonFile");
+  } else {
+    $previousFname = null;
+  }
+
+  $infoQuery = $MPC . ' --format "
+        <div class="info-container">
+          <h2>[[%title%]|[%file%]]</h2>
+          <p><strong>Artist:</strong> [%artist%]</p>
+          <p><strong>Album:</strong> [%album%]</p>" | head -n5';
+  $fnameQuery = $MPC . ' --format %file% | head -n1';
+  $currentFname = shell_exec($fnameQuery);
+
+  if ($currentFname != $previousFname){
+    file_put_contents($comparisonFile, $currentFname); 
+    $currentInfo=shell_exec($infoQuery);
+    echo $currentInfo;
+    file_put_contents($cacheFile, $currentInfo);
+  } else {
+    include($cacheFile);
+  }
+}
+
+function showBrowser(){
+  $output = shell_exec($MPC . ' ls | sort');
+  $results = preg_split('/[\r\n]+/', $output, -1, PREG_SPLIT_NO_EMPTY);
+  echo "<ul>";
+  foreach ($results as &$result) {
+    $resultEncoded=rawurlencode($result);
+    echo "
+      <li class='full'>
+        $result
+        <div class='playbtn'>
+          <svg
+            class='animated play' 
+            id='$resultEncoded' 
+            x='0px' 
+            y='0px'
+            viewBox='0 0 512 512' 
+            enable-background='new 0 0 512 512' 
+            xml:space='preserve'>
+           <g>
+            <path class='playPath' d='M508.6,148.8c0-45-33.1-81.2-74-81.2C379.2,65,322.7,64,265,64c-3,0-6,0-9,0s-6,0-9,0
+              c-57.6,0-114.2,1-169.6,3.6C36.6,67.6,3.5,104,3.5,149C1,184.6-0.1,220.2,0,255.8C-0.1,291.4,1,327,3.4,362.7
+              c0,45,33.1,81.5,73.9,81.5c58.2,2.7,117.9,3.898,178.6,3.8c60.8,0.2,120.3-1,178.6-3.8c40.9,0,74-36.5,74-81.5
+              c2.4-35.7,3.5-71.3,3.4-107C512.1,220.1,511,184.5,508.6,148.8z M207,353.9V157.4l145,98.2L207,353.9z'/>
+           </g>
+          </svg>
+        </div>
+      </li>";
+    }
+  echo "</ul>";
+}
 
 if ($_POST) {
   if (!empty($_POST["a"])) {
@@ -83,61 +142,11 @@ if (isset($GETA)) {
     break;
     case "info":
 //    authenticate() or die("access denied");
-      $cacheFile = "$cacheDir/$MPDPORT.cache";
-      $comparisonFile = "$cacheDir/$MPDPORT.comparison";
-
-      if (file_exists($comparisonFile)) {
-        $previousFname = shell_exec("cat $comparisonFile");
-      } else {
-        $previousFname = null;
-      }
-
-      $infoQuery = $MPC . ' --format "
-            <div class="info-container">
-              <h2>[[%title%]|[%file%]]</h2>
-              <p><strong>Artist:</strong> [%artist%]</p>
-              <p><strong>Album:</strong> [%album%]</p>" | head -n5';
-      $fnameQuery = $MPC . ' --format %file% | head -n1';
-      $currentFname = shell_exec($fnameQuery);
-
-      if ($currentFname != $previousFname){
-        file_put_contents($comparisonFile, $currentFname); 
-        $currentInfo=shell_exec($infoQuery);
-        echo $currentInfo;
-        file_put_contents($cacheFile, $currentInfo);
-      } else {
-        include($cacheFile);
-      }
+      showInfo();
     break;
     case "browser":
-      $output = shell_exec($MPC . ' ls | sort');
-      $results = preg_split('/[\r\n]+/', $output, -1, PREG_SPLIT_NO_EMPTY);
-      echo "<ul>";
-      foreach ($results as &$result) {
-        $resultEncoded=rawurlencode($result);
-        echo "
-          <li class='full'>
-            $result
-            <div class='playbtn'>
-              <svg
-                class='animated play' 
-                id='$resultEncoded' 
-                x='0px' 
-                y='0px'
-                viewBox='0 0 512 512' 
-                enable-background='new 0 0 512 512' 
-                xml:space='preserve'>
-               <g>
-                <path class='playPath' d='M508.6,148.8c0-45-33.1-81.2-74-81.2C379.2,65,322.7,64,265,64c-3,0-6,0-9,0s-6,0-9,0
-                  c-57.6,0-114.2,1-169.6,3.6C36.6,67.6,3.5,104,3.5,149C1,184.6-0.1,220.2,0,255.8C-0.1,291.4,1,327,3.4,362.7
-                  c0,45,33.1,81.5,73.9,81.5c58.2,2.7,117.9,3.898,178.6,3.8c60.8,0.2,120.3-1,178.6-3.8c40.9,0,74-36.5,74-81.5
-                  c2.4-35.7,3.5-71.3,3.4-107C512.1,220.1,511,184.5,508.6,148.8z M207,353.9V157.4l145,98.2L207,353.9z'/>
-               </g>
-              </svg>
-            </div>
-          </li>";
-        }
-      echo "</ul>";
+//    authenticate() or die("access denied");
+      showBrowser()
     break;
     case "test":
       $testCmd=$MPC ;
@@ -153,22 +162,21 @@ if (isset($GETA)) {
 
 if (isset($POSTA)) {
   switch ($POSTA) {
-	case "play":
-		authenticate();
-		$target=rawurldecode($POSTB);
-		$escaped=escapeshellarg($target);
-		shell_exec("$MPC clear; $MPC add " . $escaped . "; $MPC shuffle; $MPC play");
-		break;
-	case "test":
-		$testCmd=$MPC . $MPDPORT;
-		exec($testCmd,$output,$exitStatus);
-		if($exitStatus != 0) {
-			echo "error";
-		} else {
-			echo "success";
-		}
-
-		break;
+  	case "play":
+//  authenticate() or die("access denied");
+  		$target=rawurldecode($POSTB);
+  		$escaped=escapeshellarg($target);
+  		shell_exec("$MPC clear; $MPC add " . $escaped . "; $MPC shuffle; $MPC play");
+  	break;
+  	case "test":
+  		$testCmd=$MPC . $MPDPORT;
+  		exec($testCmd,$output,$exitStatus);
+  		if($exitStatus != 0) {
+  			echo "error";
+  		} else {
+  			echo "success";
+  		}
+  	break;
   }
 }
 
