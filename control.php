6f838de1d0abf0e109a8b6301a843f2bdee75527
Neil Scudder <neil@neilscudder.com>
2015-09-29 15:37:54 -0400

    fixing quoting

--- control.php
+++ control.php
@@ -41,7 +41,7 @@ function showInfo(){
         <h2>[[%title%]|[%file%]]</h2>
         <p><strong>Artist:</strong> [%artist%]</p>
         <p><strong>Album:</strong> [%album%]</p>
-        <div class='animated button' id='insertNextTwo'>
+        <div class="animated button" id="insertNextTwo">
             Insert Next Two
         </div>
       </div>" | head -n5';
