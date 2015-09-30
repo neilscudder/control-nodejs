8ae947aa1a5c216e4d7605772cd6f6a610ff4e2b
Neil Scudder <neil@neilscudder.com>
2015-09-30 18:58:49 -0400

    still debugging the shell script

--- insertNextTwo.sh
+++ insertNextTwo.sh
@@ -2,16 +2,19 @@
 
 main()
 {
-  $MPC="$1"
-  local currentAlbum=$($MPC --format %album% | head -n 1)
-  local currentFname=$($MPC --format %file% | head -n 1)
-  local albumFnames=$($MPC --format %file% search album "$currentAlbum")
-  local nextTwoFnames=$($albumFnames | grep -A 2 "$currentFname")
-  if [ $(echo "$nextTwoFnames" | wc -l) -gt 2 ]; then
+  MPC="$1"
+  local currentAlbum="$($MPC --format %album% | head -n 1)"
+  local currentFname="$($MPC --format %file% | head -n 1)"
+  local albumFnames="$($MPC --format %file% search album "$currentAlbum")"
+  echo "one"
+  local nextTwoFnames="$(echo "$albumFnames" | grep -A 2 "$currentFname")"
+  echo "two"
+  if [ $(echo "$nextTwoFnames" | wc -l) -gt 1 ]; then
     $MPC insert "$nextTwoFnames"
+    echo "three"
   fi
 }
 
-main("$1")
+main "$1"
 
 exit 0
