c811f10f932c5c936cce47b2b4b1757aa207e1c2
Neil Scudder <neil@neilscudder.com>
2015-10-01 01:24:53 -0400

    almoat works

--- insertNextTwo.sh
+++ insertNextTwo.sh
@@ -7,10 +7,14 @@ main()
   local currentFname="$($MPC --format %file% | head -n 1)"
   local albumFnames="$($MPC --format %file% search album "$currentAlbum")"
   echo "one"
-  local nextTwoFnames="$(echo "$albumFnames" | grep -A 2 "$currentFname")"
-  echo "two"
-  if [ $(echo "$nextTwoFnames" | wc -l) -gt 1 ]; then
-    $MPC insert "$nextTwoFnames"
+  local nextFnames="${albumFnames##"$currentFname"}"
+#  echo "$nextFnames"
+#  IFS=$'\n' read -r -d -a nextArray <<< "$nextFnames"
+  readarray -t nextArray <<<"$nextFnames"
+  echo "${nextArray["0"]}"
+  echo "${nextArray[1]}"
+  if [ ${#nextArray[@]} -gt 1 ]; then
+    $MPC insert "${nextArray[0]}" "${nextArray[1]}"
     echo "three"
   fi
 }
