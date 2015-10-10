68a14230fd94658ea46b24e6d1f358c16f262e04
Neil Scudder <neil@neilscudder.com>
2015-10-10 17:26:10 -0400

    added https server

--- ctrl.js
+++ ctrl.js
@@ -1,7 +1,21 @@
+var https = require('https')
+var fs = require('fs')
 var childProcess = require('child_process')
-childProcess.exec('/usr/bin/mpc -p 1027 -h user@localhost', mpdCallback)
 
-function mpdCallback(error, stdout, stderr) {
-  console.log(stdout)
+function mpdStatus() {
+  childProcess.exec('/usr/bin/mpc -p 1027 -h user@localhost', mpdCallback)
+  function mpdCallback(error, stdout, stderr) {
+    return stdout
+  }
 }
 
+var options = {
+  key: fs.readFileSync('/etc/ssl/private/playnode.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/playnode.pem')
+}
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200)
+//  res.end(mpdStatus())
+  res.end("yo")
+}).listen(8000, "0.0.0.0")
