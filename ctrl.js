02f532d724155d416736dd2dd2adf316337bb4a4
Neil Scudder <neil@neilscudder.com>
2015-10-10 19:27:42 -0400

    Parsing get now

--- ctrl.js
+++ ctrl.js
@@ -1,6 +1,7 @@
 var https = require('https')
   , fs = require('fs')
   , childProcess = require('child_process')
+  , url = require('url')
 
 function mpdStatus() {
   childProcess.exec('/usr/bin/mpc -p 1027 -h user@localhost', mpdCallback)
@@ -16,8 +17,18 @@ var options = {
 }
 
 https.createServer(options, function (req, res) {
-  var statusSnapshot = String(mpdStatus())
-  res.writeHead(200)
-  res.write(statusSnapshot)
-  res.end()
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query
+  switch(query['a']) {
+    case 'up':
+      res.writeHead(200)
+      res.write('up')
+      res.end()
+    break;
+    default:
+      res.writeHead(200)
+      res.write('default')
+      res.end()
+    break;
+  }
 }).listen(8000, "0.0.0.0")
