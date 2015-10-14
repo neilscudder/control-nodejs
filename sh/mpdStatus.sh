a29be21491276b26ba68f9ad89c504900e908eab
Neil Scudder <neil@neilscudder.com>
2015-10-14 19:00:06 -0400

    adding some untracked stuff

---
+++ sh/mpdStatus.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+MPC="$1"
+$MPC --format \
+"<div class=\"info-container\"> \
+  <h2>[[%title%]|[%file%]]</h2> \
+  <p><strong>Artist:</strong> [%artist%]</p> \
+  <p><strong>Album:</strong> [%album%]</p> \
+  <div class=\"animated button released\" id=\"insertNextTwo\"> \
+    Insert Next Two \
+  </div> \
+</div>" | head -n1
+exit 0
