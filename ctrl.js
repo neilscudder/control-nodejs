2472bf9fd1c6c7b1a69ffa3995fcb9f92499c3be
Neil Scudder <neil@neilscudder.com>
2015-10-25 04:38:35 -0400

    moving server callback function out to global scope

--- ctrl.js
+++ ctrl.js
@@ -14,10 +14,11 @@ var options = {
 
 var mongourl = 'mongodb://webserver:webmunster@localhost/authority'
 
-https.createServer(options, function (req, res) {
+https.createServer(options, start).listen(8000, "0.0.0.0")
+
+function start(req, res) {
   var url_parts = url.parse(req.url, true)
   var query = url_parts.query
-
   MongoClient.connect(mongourl, function(err, db) {
     assert.equal(null, err)
     authenticate(db, query['k'] , function() {
@@ -31,55 +32,60 @@ https.createServer(options, function (req, res) {
         assert.equal(err, null)
         if (doc != null) {
            console.dir(doc)
-           proceed()
+           proceed(req, res)
         } else {
            callback()
         }
      })
   }
-  function proceed() {
-    var mpc = '/usr/bin/mpc'
-    mpc += ' -h ' + query['p']
-    mpc += '@' + query['h'] 
-    mpc += ' -p ' + query['m'] 
-    mpc += ' '
-    res.statusCode = 200
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    switch(query['a']) {
-      case 'info':
-        res.setHeader("Content-Type","text/html") 
-        var cmd = 'sh/mpdStatus.sh ' + '"' + mpc + '"'
-        childProcess.exec(cmd,theEnd)
-      break;
-      case 'up':
-        mpc += ' volume +5'
-        res.setHeader("Content-Type","text/html") 
-        var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
-        childProcess.exec(cmd,theEnd)
-      break;
-      case 'dn':
-        mpc += ' volume -5'
-        res.setHeader("Content-Type","text/html") 
-        var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
-        childProcess.exec(cmd,theEnd)
-      break;
-      case 'fw':
-        mpc += ' next'
-        childProcess.exec(mpc,theEnd)
-      break;
-      default:
-        result = 'default'
-        theEnd()
-    }
-    function theEnd(err,stdout,stderr){
-      if (err) {
-        console.log(err)
-      } else if (stdout) {
-        res.end(stdout,'utf8')
-      } else {
-        res.end
-      }
-      if (stderr) console.log(stderr)
-    }
+}
+
+
+function proceed(req, res) {
+  var url_parts = url.parse(req.url, true)
+  var query = url_parts.query
+  var mpc = '/usr/bin/mpc'
+  mpc += ' -h ' + query['p']
+  mpc += '@' + query['h'] 
+  mpc += ' -p ' + query['m'] 
+  mpc += ' '
+  res.statusCode = 200
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  switch(query['a']) {
+    case 'info':
+      res.setHeader("Content-Type","text/html") 
+      var cmd = 'sh/mpdStatus.sh ' + '"' + mpc + '"'
+      childProcess.exec(cmd,theEnd)
+    break;
+    case 'up':
+      mpc += ' volume +5'
+      res.setHeader("Content-Type","text/html") 
+      var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
+       childProcess.exec(cmd,theEnd)
+    break;
+    case 'dn':
+      mpc += ' volume -5'
+      res.setHeader("Content-Type","text/html") 
+      var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
+      childProcess.exec(cmd,theEnd)
+    break;
+    case 'fw':
+      mpc += ' next'
+      childProcess.exec(mpc,theEnd)
+    break;
+    default:
+      result = 'default'
+      theEnd()
   }
-}).listen(8000, "0.0.0.0")
+
+  function theEnd(err,stdout,stderr){
+    if (err) {
+      console.log(err)
+    } else if (stdout) {
+      res.end(stdout,'utf8')
+    } else {
+      res.end
+     }
+    if (stderr) console.log(stderr)
+  }
+}
