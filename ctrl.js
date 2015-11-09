1e66f1845f19b79ea7889ae44cafe360d88cd97e
Neil Scudder <neil@neilscudder.com>
2015-11-09 13:35:21 -0500

    Restructuring

--- ctrl.js
+++ ctrl.js
@@ -13,97 +13,90 @@ var options = {
       key: fs.readFileSync('../.ssl/private/playnode.key'),
       cert: fs.readFileSync('../.ssl/playnode.pem')
     }
-  , port = 443
+  , serverListenPort = 443
 
-https.createServer(options, authenticate).listen(port, "0.0.0.0")
-
-function router(req, res) {
-  // First step to replace authenticate
+https.createServer(options, function(){
   var url_parts = url.parse(req.url, true)
   var query = url_parts.query
-}
 
-function gui(req, res) {
-  var url_parts = url.parse(req.url, true)
-  var query = url_parts.query
-  // Assemble the html and return it
-  var fn = jade.compile('index.jade')
-  var htmlOutput = fn({
-    control: {
-      mpdport: query['m'],
-      mpdhost: query['h'],
-      mpdpass: query['p']
+  function authenticate() {
+    var mongourl = 'mongodb://webserver:webmunster@localhost/authority'
+    MongoClient.connect(mongourl, function(err, db) {
+      assert.equal(null, err)
+      lookupKey(db, query['k'] , function() {
+          db.close()
+      })
+    }) 
+    var lookupKey = function(db, key, callback) {
+       var cursor =db.collection('playnodeca').find( { "KPASS": key } )
+       cursor.each(function(err, doc) {
+          assert.equal(err, null)
+          if (doc != null) {
+             console.dir(doc)
+             processRequest(req, res)
+          } else {
+             callback()
+          }
+       })
     }
-  })
-}
+  }
 
-function authenticate(req, res) {
-  var mongourl = 'mongodb://webserver:webmunster@localhost/authority'
-  var url_parts = url.parse(req.url, true)
-  var query = url_parts.query
-  MongoClient.connect(mongourl, function(err, db) {
-    assert.equal(null, err)
-    lookupKey(db, query['k'] , function() {
-        db.close()
+  function gui() {
+    // Assemble the html and return it
+    var fn = jade.compile('index.jade')
+    var htmlOutput = fn({
+      control: {
+        mpdport: query['m'],
+        mpdhost: query['h'],
+        mpdpass: query['p']
+      }
     })
-  })
-  var lookupKey = function(db, key, callback) {
-     var cursor =db.collection('playnodeca').find( { "KPASS": key } )
-     cursor.each(function(err, doc) {
-        assert.equal(err, null)
-        if (doc != null) {
-           console.dir(doc)
-           processRequest(req, res)
-        } else {
-           callback()
-        }
-     })
   }
-}
-function processRequest(req, res) {
-  var url_parts = url.parse(req.url, true)
-  var query = url_parts.query
-  var mpc = '/usr/bin/mpc'
-  mpc += ' -h ' + query['p']
-  mpc += '@' + query['h'] 
-  mpc += ' -p ' + query['m'] 
-  mpc += ' '
-  res.statusCode = 200
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  switch(query['a']) {
-    case 'info':
-      res.setHeader("Content-Type","text/html") 
-      var cmd = 'sh/mpdStatus.sh ' + '"' + mpc + '"'
-      childProcess.exec(cmd,returnData)
-    break;
-    case 'up':
-      mpc += ' volume +5'
-      res.setHeader("Content-Type","text/html") 
-      var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
-       childProcess.exec(cmd,returnData)
-    break;
-    case 'dn':
-      mpc += ' volume -5'
-      res.setHeader("Content-Type","text/html") 
-      var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
-      childProcess.exec(cmd,returnData)
-    break;
-    case 'fw':
-      mpc += ' next'
-      childProcess.exec(mpc,returnData)
-    break;
-    default:
-      result = 'default'
-      returnData()
-  }
-  function returnData(err,stdout,stderr){
-    if (err) {
-      console.log(err)
-    } else if (stdout) {
-      res.end(stdout,'utf8')
-    } else {
-      res.end
-     }
-    if (stderr) console.log(stderr)
-  }
-}
+
+  function processRequest() {
+    var mpc = '/usr/bin/mpc'
+    mpc += ' -h ' + query['p']
+    mpc += '@' + query['h']
+    mpc += ' -p ' + query['m']
+    mpc += ' '
+    res.statusCode = 200
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    switch(query['a']) {
+      case 'info':
+	res.setHeader("Content-Type","text/html")
+	var cmd = 'sh/mpdStatus.sh ' + '"' + mpc + '"'
+	childProcess.exec(cmd,returnData)
+      break;
+      case 'up':
+	mpc += ' volume +5'
+	res.setHeader("Content-Type","text/html")
+	var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
+	 childProcess.exec(cmd,returnData)
+      break;
+      case 'dn':
+	mpc += ' volume -5'
+	res.setHeader("Content-Type","text/html")
+	var cmd = 'sh/volume.sh ' + '"' + mpc + '"'
+	childProcess.exec(cmd,returnData)
+      break;
+      case 'fw':
+	mpc += ' next'
+	childProcess.exec(mpc,returnData)
+      break;
+      default:
+	result = 'default'
+	returnData()
+    }   
+    function returnData(err,stdout,stderr){
+      if (err) {
+	console.log(err)
+      } else if (stdout) {
+	res.end(stdout,'utf8')
+      } else {
+	res.end
+       }
+      if (stderr) console.log(stderr)
+    } 
+  } 
+}).listen(serverListenPort, "0.0.0.0")
+
